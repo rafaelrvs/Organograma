768675dcd33fdfd5c43e4dc0e32987ef420197d2
rafaelrvs <rafaelvieira1333@gmail.com>
2023-07-11 14:00:50 -0300

    feat

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,9 +1,10 @@
-const CampoTexto = () =>{
+import "./CampoTexto.css";
+const CampoTexto = (props) =>{
+const placeholderModificado = `${props.placeholder}...`
   return(
-    <div>
-      <label>Nome</label>
-      <input type="text" />
-
+    <div className="labelInput">
+      <label>{props.label}</label>
+      <input placeholder={placeholderModificado} />
     </div>
   )
 }
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,14 @@
+import CampoTexto from "../CampoTexto";
+import "./Formulario.css";
+const Formulario = () => {
+  return(
+    <section>
+    <form>
+      <CampoTexto label="Nome" placeholder="Digite seu nome" />
+      <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+    </form>
+  </section>
+)
+};
+export default Formulario;
